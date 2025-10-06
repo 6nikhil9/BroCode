e07fcd7cf5586c89fe8c6e52ef7b42b244c9da5c
Your Name <you@example.com>
2025-10-06 14:49:22 +0530

    patttern

---
+++ C++/pattern.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int n;
+// int main(){
+//     cin>>n;
+//     int spaces= 2*n-2;
+//     for(int i=1;i<=2*n-1;i++){
+//     int stars=i;
+//     if(i>n) stars = 2*n-i;
+//     //stars
+//     for(int j=1;j<=stars;j++){
+//         cout<<"*";
+//     }
+//     //spaces
+//     for(int j=1;j<=spaces;j++){
+//         cout<<"";
+//     }
+//     //stars
+//     for(int j=1;j<=stars;j++){
+//         cout<<"*";
+//     }
+//     cout<<endl;
+//     if (i<n) spaces -=2;
+//     else spaces+=2;
+//     }
+// }
+
+// int main(){
+//     cin>>n;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//             if(i==0 || j==0 || i==n-1 || j==n-1){
+//                 cout<<"*";
+//             }
+//             else cout<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// int main(){
+//     cin>>n;
+//     for(int i=0;i<2*n-1;i++){
+//         for(int j=0;j<2*n-1;j++){
+//             int top=i;
+//             int left=j;
+//             int right=(2*n-2)-j;
+//             int down=(2*n-2)-i;
+//             cout<<(n-min(min(top,down), min(left,right)));
+//         }
+//         cout<<endl;
+//     }
+// }
+
